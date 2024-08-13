5aa001fa6b8a06a86cff29aa866c9904429aac55
LAPTOP-KCAUDTIJ\FIRDAUS <firdauschuzaeni@gmail.com>
2024-08-13 18:45:37 +0700

    new create scheema prisma member

---
+++ prisma/migrations/20240813092701_members/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `members` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(225) NULL,
+    `email` VARCHAR(225) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
