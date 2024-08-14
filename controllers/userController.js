146fcc720f607ad598fc9769a93fdfc1e7aec5e3
LAPTOP-KCAUDTIJ\FIRDAUS <firdauschuzaeni@gmail.com>
2024-08-14 16:02:31 +0700

    remove comment

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,47 +42,47 @@ const findUsers = async (req, res) => {
 };
 
 //function createUser
-// const createUser = async (req, res) => {
+const createUser = async (req, res) => {
 
-//     // Periksa hasil validasi
-//     const errors = validationResult(req);
+    // Periksa hasil validasi
+    const errors = validationResult(req);
 
-//     if (!errors.isEmpty()) {
-//         // Jika ada error, kembalikan error ke pengguna
-//         return res.status(422).json({
-//             success: false,
-//             message: "Validation error",
-//             errors: errors.array(),
-//         });
-//     }
+    if (!errors.isEmpty()) {
+        // Jika ada error, kembalikan error ke pengguna
+        return res.status(422).json({
+            success: false,
+            message: "Validation error",
+            errors: errors.array(),
+        });
+    }
 
-//     //hash password
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    //hash password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-//     try {
+    try {
 
-//         //insert data
-//         const user = await prisma.user.create({
-//             data: {
-//                 name: req.body.name,
-//                 email: req.body.email,
-//                 password: hashedPassword,
-//             },
-//         });
+        //insert data
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+            },
+        });
 
-//         res.status(201).send({
-//             success: true,
-//             message: "User created successfully",
-//             data: user,
-//         });
+        res.status(201).send({
+            success: true,
+            message: "User created successfully",
+            data: user,
+        });
 
-//     } catch (error) {
-//         res.status(500).send({
-//             success: false,
-//             message: "Internal server error",
-//         });
-//     }
-// };
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
 
 //function findUserById
 const findUserById = async (req, res) => {
